783746943c13fd0c5b8be61f406ff95455823e2d
[Acejack7] <[adasio7@gmail.com]>
2019-02-16 21:12:54 +0100

    excel contents of source and target ready;

--- excel_comparison.py
+++ excel_comparison.py
@@ -33,7 +33,7 @@ def get_files(file_path, type):
         else:
             lang_code = ''
 
-        return(file_list, cur_dir, lang_code)
+        return({'files': file_list, 'cur_dir': cur_dir, 'lang_code': lang_code})
 
     elif os.path.isfile(file_path):
         file_path_split = file_path.split('\\')
@@ -52,8 +52,52 @@ def get_files(file_path, type):
         else:
             lang_code = ''
 
-        return(file_path, cur_dir, lang_code)
+        return({'files': [file_path], 'cur_dir': cur_dir, 'lang_code': lang_code})
 
 
+def get_target_lang(lang_code):
+    if lang_code in languages:
+        return(languages[lang_code])
+    else:
+        return('')
 
-        
+
+def get_excel_contents(files, target_lang):
+
+    segments = []
+
+    for file_elem in files:
+        wb = load_workbook(file_elem)
+
+        ws = wb.worksheets
+
+        for sheet in ws:
+            if sheet.sheet_state == 'visible':
+                for row in sheet.rows:
+                    for cell in row:
+                        cell_value = str(cell.value).lower()
+                        if cell_value.startswith('english') or cell_value.startswith('source'):
+                            start_row = cell.row + 1
+                            source_col = cell.column
+                            break
+                    else:
+                        continue
+                    break
+
+                for col in sheet.iter_cols(min_row=start_row, max_row=start_row):
+                    for cell in col:
+                        cell_value = str(cell.value).lower()
+                        if cell_value.startswith('translation') or cell_value.startswith('target'):
+                            target_col = cell.column
+                            break
+                        elif cell_value in target_lang:
+                            target_col = cell.column
+                            break
+                
+                for num in range(start_row, sheet.max_row):
+                    source_content = sheet[source_col + str(num)].value
+                    target_content = sheet[target_col + str(num)].value
+                    if source_content is not None and target_content is not None:
+                        segments.append({'source': source_content, 'target': target_content})
+    
+    return(segments)
